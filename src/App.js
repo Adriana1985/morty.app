a83afb8ee5cb6557e742347119c424349f25829c
Adriana1985 <adriana.uaem@gmail.com>
2023-05-28 13:15:51 -0600

    primer commit morty

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import imagen1 from '../src/img/rick-morty.png'
+import Characters from './Components/Characters';
+import { useState } from 'react';
+function App() {
+  const[characters,setCharacters] = useState(null);
+  const reqApi = async () => {
+    const api = await fetch("https://rickandmortyapi.com/api/character");
+
+    const characterApi = await api.json();
+
+    setCharacters(characterApi.results);
+    console.log(characters);
+  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className='title'>Rick & Morty</h1>
+        {characters ? (   
+        <Characters characters={characters}/>
+         ):(
+        <>  
+          <img src={imagen1} alt='Rick and Morty' className='img-home'/>
+          <button onClick={reqApi} className='btn-search'>
+            Buscar Personajes
+          </button>
+        </>  
+       )  
+      }
+      </header>
+    </div>
+  );
+}
+
+export default App;
